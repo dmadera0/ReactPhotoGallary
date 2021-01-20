3621fe0e9f0f028f585be5b1f814521549c3b612
Daniel <dmadera0@gmail.com>
2021-01-20 15:56:15 -0700

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className = "title">
+            <h1>Daniel Photos</h1>
+            <h2>Photo Gallary</h2>
+            <p>The world the way I see it</p>
+        </div>
+    )
+}
+
+export default Title;
